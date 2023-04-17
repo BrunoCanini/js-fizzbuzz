bdb2fb9c0e92d025a5236ee3d4f604bfb2081928
Bruno Canini <brunocanini1994@gmail.com>
2023-04-17 16:04:01 +0200

    modulo

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ for (let i = 1; i <= 100; i++) {
     
     console.log(i);
 
-    if((i / 3)!=0){
+    if((i % 3)!=0){
         console.log("fizz")
-    } else if ((i / 5)!=0){
+    } else if ((i % 5)!=0){
         console.log("buzz")
     } else{
         console.log(i)
