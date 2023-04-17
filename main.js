2802af6ab64220174bd7f15dd9538e843f1d0d9d
Bruno Canini <brunocanini1994@gmail.com>
2023-04-17 17:56:57 +0200

    fizz, buzz , -fizz-buzz

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 for (let i = 1; i <= 100; i++) {
 
-    let n = (i % 3)==0;
-    let m = (i % 5)==0;
-    if(n){
+    let multiTre = (i % 3)==0;
+    let multiCin = (i % 5)==0;
+    
+    if(multiTre && multiCin ){
+        console.log ("fizz-buzz")
         console.log("fizz")
-    } else if (m){
+    } else if (multiCin ){
         console.log("buzz")
-    } else if ( n && m){
-        console.log ("fizz buzz")
+    } else if (multiTre){
+        console.log("fizz")
     } else{
         console.log(i);
     }
