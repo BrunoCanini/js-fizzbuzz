aff6023f961c548bfc078ae0efc3c41cef785b7a
Bruno Canini <brunocanini1994@gmail.com>
2023-04-17 17:29:18 +0200

    fizz - buzz

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 for (let i = 1; i <= 100; i++) {
 
-    console.log(i);
-
     let n = (i % 3)==0;
     let m = (i % 5)==0;
-
     if(n){
         console.log("fizz")
     } else if (m){
         console.log("buzz")
     } else if ( n && m){
         console.log ("fizz buzz")
+    } else{
+        console.log(i);
     }
 }
